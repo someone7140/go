ea2bf5623cee59ff8c6259768b2e9364d643b02a
someone7140 <ti7140@mail.goo.ne.jp>
2021-01-20 02:18:26 +0900

    ArraySampleの追加

---
+++ practice/go.mod
@@ -0,0 +1,8 @@
+module practice
+
+go 1.14
+
+require (
+	github.com/adam-lavrik/go-imath v0.0.0-20200104135348-19d2b923d06c
+	github.com/thoas/go-funk v0.7.0
+)
---
+++ practice/maxArray.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/adam-lavrik/go-imath/ix"
+	"github.com/thoas/go-funk"
+)
+
+func main() {
+	sampleArray := []int{1, 6, 2, 10, 9, 14, 100, 5}
+	// 愚直にループを回す
+	max := -9999999
+	for _, s := range sampleArray {
+		if max < s {
+			max = s
+		}
+	}
+	fmt.Println(max)
+
+	// go-imathのライブラリを使う
+	max = -9999999
+	max = ix.MaxSlice(sampleArray)
+	fmt.Println(max)
+
+	//go-funkのライブラリを使う
+	max = -9999999
+	max = funk.MaxInt(sampleArray).(int)
+	fmt.Println(max)
+}
